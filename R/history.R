a61662f41fd42862f6313e82420fa789de065741
Serhij S <div@altertech.com>
2023-03-29 15:20:44 +0200

    import

---
+++ R/history.R
@@ -0,0 +1,171 @@
+#' Create a new item state history request
+#'
+#' @param params_csv Load oid map params from a CSV file
+#'
+#' @export
+eva.history.request <- function(params_csv=NULL) {
+  request <- list(params = list(fill="1T"), oid_map = list())
+  if (!is.null(params_csv)) {
+    params <- read.csv(params_csv)
+    for (i in 1:nrow(params)) {
+      row <- params[i,]
+      request <- eva.history.append_oid(request, row$oid, row$status, row$value, row$database)
+    }
+  }
+  return(request)
+}
+#' Append item OID mapping to a request
+#'
+#' @param request
+#'
+#' @param oid item OID
+#' @param status status mapping: TRUE or col name
+#' @param value value mapping: TRUE or col name
+#' @param database database service to load state from
+#' @param xopts extra database request options
+#'
+#' @export
+eva.history.append_oid <- function(request, oid, status=FALSE, value=FALSE, database=NULL, xopts=NULL) {
+  p <- list(status=status, value=value)
+  if (!is.null(database)) p["database"] <- database
+  if (!is.null(xopts)) p["xopts"] <- xopts
+  request$oid_map[oid] <- list(p)
+  return(request)
+}
+#' Fetch state history data
+#'
+#' @param session
+#'
+#' @param request Request object
+#' @param t_start start time: date/time or unix timestamp
+#' @param t_end end time: date/time or unix timestamp
+#' @param fill fill: NX, where X: S for seconds, T for minutes, H for hours, D for days, W for weeks, e.g. 15T
+#' @param limit limit result records to
+#' @param database database service to perform request on
+#' @param xopts extra options
+#' @param t_col time column processing: keep (default) or drop
+#' @param tz time zone
+#'
+#' @import httr
+#' @import jsonlite
+#' @import tibble
+#' @import readr
+#' @export
+eva.history.fetch <- function(session, request,
+                              t_start=NULL, t_end=NULL, fill=NULL, limit=NULL, database=NULL, xopts=NULL,
+                              t_col="keep", tz=NULL) {
+  ml_url <- session$mlkit
+  params <- request$params
+  if (!is.null(t_start)) params$t_start <- t_start
+  if (!is.null(t_end)) params$t_end <- t_end
+  if (!is.null(fill)) params$fill <- fill
+  if (!is.null(limit)) params$limit <- limit
+  if (ml_url == FALSE) {
+    # fetch data from HMI
+    params$i <- names(request$oid_map)
+    if (!is.null(xopts)) params$xopts <- xopts
+    if (!is.null(database)) params$database <- database
+    stat <- eva.call(session, "item.state_history", params)
+    if (t_col == "keep") {
+      data <- tibble(time=stat$t)
+      data$time <- as.POSIXct(data$time, origin="1970-01-01", tz=tz)
+    } else data <- tibble(time=matrix(NA, ncol=1, nrow=length(stat$t)))
+    for (oid in names(request$oid_map)) {
+      p <- request$oid_map[[oid]]
+      if (isTRUE(p$status) || identical(p$status,"true") || identical(p$status, 1) || identical(p$status, "1")) {
+        col_name <- paste(oid, "status", sep="/")
+        data[col_name] <- stat[col_name]
+      } else if (is.character(p$status)) {
+        col_name <- paste(oid, "status", sep="/")
+        data[p$status] <- stat[col_name]
+      }
+      if (isTRUE(p$value) || identical(p$value,"true") || identical(p$value, 1) || identical(p$value, "1")) {
+        col_name <- paste(oid, "value", sep="/")
+        data[col_name] <- stat[col_name]
+      } else if (is.character(p$value)) {
+        col_name <- paste(oid, "value", sep="/")
+        data[p$value] <- stat[col_name]
+      }
+    }
+    if (t_col != "keep") data <- data[,-1]
+    return(data)
+  } else {
+    if (isTRUE(ml_url)) ml_url <- session$url
+    url <- paste(ml_url, "ml/api/query.item.state_history", sep="/")
+    if (t_col == "drop") params$time_format <- "no" else params$time_format <- "raw"
+    if (is.null(session$token)) {
+      eva.authenticate(session)
+      refreshed <- TRUE
+    } else refreshed <- FALSE
+    repeat {
+      params$oid_map <- request$oid_map
+      response <- POST(url, content_type_json(),
+                       body = jsonlite::toJSON(params, auto_unbox=TRUE),
+                       stream = TRUE,
+                       add_headers("x-auth-key" = session$token),
+                       add_headers("accept-encoding" = "gzip"), timeout(session$timeout))
+      if (status_code(response) == 403 && endsWith(content(response), "(AUTH)")) {
+        if (refreshed) break else {
+          eva.authenticate(session)
+          refreshed <- TRUE
+        }
+      } else break
+    }
+    if (status_code(response) != 200) stop(paste(status_code(response), content(response)))
+    data = read_csv(response$content, show_col_types=FALSE)
+    if (t_col == "keep") data$time <- as.POSIXct(data$time, origin="1970-01-01", tz=tz)
+    return(data)
+  }
+}
+
+#' Push data to a database service
+#'
+#' @param session
+#'
+#' @param request request object
+#' @param data file path or a data frame
+#' @param database database to push data to (default: default)
+#'
+#' @import curl
+#' @import httr
+#' @import jsonlite
+#' @export
+eva.history.push <- function(session, request, data, database="default") {
+  c <- NULL
+  out <- tryCatch(
+                  {
+                    ml_url <- session$mlkit
+                    if (isFALSE(ml_url)) stop("the operation requires ml kit server")
+                    if (isTRUE(ml_url)) ml_url <- session$url
+                    params = list(params = jsonlite::toJSON(list(database = database, oid_map = request$oid_map), auto_unbox=TRUE))
+                    if (typeof(data) == "character") {
+                      params$file = upload_file(data)
+                    } else if (typeof(data) == "list") {
+                      c <- textConnection("csv", "w")
+                      write.csv(data, c, row.names=FALSE)
+                      params$file = curl::form_data(textConnectionValue(c), type="application/csv")
+                    } else stop("unsupported data kind")
+                    if (is.null(session$token)) {
+                      eva.authenticate(session)
+                      refreshed <- TRUE
+                    } else refreshed <- FALSE
+                    url <- paste(ml_url, "ml/api/push.item.state_history_csv", sep="/")
+                    repeat {
+                      response <- POST(url, body = params, encode = "multipart",
+                                       add_headers("x-auth-key" = session$token), timeout(session$timeout))
+                      if (status_code(response) == 403 && endsWith(content(response), "(AUTH)")) {
+                        if (refreshed) break else {
+                          eva.authenticate(session)
+                          refreshed <- TRUE
+                        }
+                      } else break
+                    }
+                    if (status_code(response) != 200) stop(paste(status_code(response), content(response)))
+                    return(content(response))
+                  },
+                  finally={
+                    if (!is.null(c)) close(c)
+                  }
+  )
+  return(out)
+}
---
+++ R/http_client.R
@@ -0,0 +1,99 @@
+#' Create a new EVA ICS session
+#'
+#' @param url HMI or front-end server URL
+#'
+#' @param user user login (for login/password authentication)
+#' @param password user password
+#' @param key API key (for API key authentication)
+#' @param timeout timeout for HTTP requests
+#' @param mlkit if ML Kit server is installed: TRUE (if front-end server is used) or ML Kit server URL
+#'
+#' @export
+eva.session <- function(url="http://localhost:7727",
+                        user=NULL,
+                        password=NULL,
+                        key=NULL,
+                        timeout=120,
+                        mlkit=FALSE) {
+  session <- new.env()
+  session$url <- url
+  session$call_id <- 1
+  session$user <- user
+  session$password <- password
+  session$key <- key
+  session$token <- NULL
+  session$mlkit <- mlkit
+  session$timeout <- timeout
+  return(session)
+}
+eva.get_call_id <- function(session) {
+  session$call_id <- session$call_id + 1
+  if (session$call_id > 0xFFFFFFFF) session$call_id <- 1;
+  return(session$call_id)
+}
+#' Set session credentials for login/password authentication
+#'
+#' @param session
+#'
+#' @param user user login
+#' @param password user password
+#'
+#' @export
+eva.credentials <- function(session, user, password) {
+  session$user <- user;
+  session$password <- password;
+}
+#' Authenticate the session (usually not called manually)
+#'
+#' @param session
+#'
+#' @export
+eva.authenticate <- function(session) {
+  if (is.null(session$user) || is.null(session$password)) stop("credentials not set")
+  r <- eva.call(session, "login", list(u = session$user, p = session$password))
+  session$token <- r$token
+}
+#' Perform a RPC call on HMI service
+#'
+#' @param session
+#'
+#' @param method RPC method
+#' @param params method params
+#'
+#' @import httr
+#' @import jsonlite
+#' @export
+eva.call <- function(session, method, params=list()) {
+  call_id <- eva.get_call_id(session)
+  token_auth <- FALSE
+  if (method != "login") {
+    if (!is.null(session$key)) params$k <- session$key else if (!is.null(session$token)) {
+      params$k <- session$token
+      token_auth <- TRUE
+    } else {
+      eva.authenticate(session)
+      params$k <- session$token
+    }
+  }
+  req <- list(jsonrpc = "2.0", id = call_id, method = method, params = params)
+  r <- POST(session$url, content_type_json(), body = jsonlite::toJSON(req, auto_unbox=TRUE),
+            timeout(session$timeout))
+  if (status_code(r) != 200) stop(paste("http error", status_code(r)))
+  ct <- jsonlite::fromJSON(content(r, as="text", encoding="UTF-8"))
+  if (ct$id != call_id) stop("invalid API response ID")
+  if (!is.null(ct$error)) {
+    if (ct$error$code == -32002 && token_auth) {
+      session$token <- NULL
+      return(eva.call(method, params))
+    } else stop(paste(ct$error$code, ct$error$message))
+  }
+  return(ct$result)
+}
+#' Execute "test" RPC method on HMI service
+#'
+#' @param session
+#'
+#' @export
+eva.test <- function(session) {
+  return(eva.call(session, "test"))
+}
